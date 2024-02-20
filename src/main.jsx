062457ea73c5d4e2727292b4624ceebfadd639b7
dom <35171942+domluther@users.noreply.github.com>
2024-02-20 23:57:06 +0000

    cleaned import

--- src/main.jsx
+++ src/main.jsx
@@ -1,7 +1,7 @@
 /*eslint no-unused-vars: "warn"*/
 /*eslint react/prop-types: "off"*/
 
-import React, { useState } from 'react';
+import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App.jsx';
 import './index.css';
